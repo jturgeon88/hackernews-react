fae884e186f9bfdb3c90889dfc3c55cfff9a800d
Josh Turgeon <jturgeon88@gmail.com>
2019-09-11 15:13:55 -0500

    add a header and react routing for LinkList and CreateLink

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 const httpLink = createHttpLink({
@@ -20,9 +21,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
   document.getElementById('root')
 )
 serviceWorker.unregister();
